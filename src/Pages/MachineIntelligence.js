31daf2d8b3838638ac4398e53432f0cdde47fb4e
Sarah <lemiex@outlook.com>
2022-08-02 19:31:53 -0400

    first commit

---
+++ src/Pages/MachineIntelligence.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+const MachineIntelligence = () => {
+
+    function onLoad
+    return (
+        <Spline scene="https://prod.spline.design/jpi11Nh8wnGc9BM2/scene.splinecode" />
+    ); s
+}
+
+export default MachineIntelligence
---
+++ src/Pages/Sustainability.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+const Sustainability = () => {
+    return (
+        <Spline scene="https://prod.spline.design/9g2RONVL5FFMxjXu/scene.splinecode" />
+    );
+}
+
+export default Sustainability
