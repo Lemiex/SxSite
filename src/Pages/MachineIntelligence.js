8c34a8afb47afa4cb536061add3a33fe25e2d3c9
Sarah <lemiex@outlook.com>
2022-08-03 15:21:28 -0400

    added clicking animation

--- src/Pages/MachineIntelligence.js
+++ src/Pages/MachineIntelligence.js
@@ -3,10 +3,10 @@ import Spline from '@splinetool/react-spline';
 
 const MachineIntelligence = () => {
 
-    function onLoad
+
     return (
         <Spline scene="https://prod.spline.design/jpi11Nh8wnGc9BM2/scene.splinecode" />
-    ); s
+    );
 }
 
 export default MachineIntelligence
