d2b03fe98b692433a8b8ff556ecf1efad2e79fa1
Sarah <lemiex@outlook.com>
2022-08-03 17:56:09 -0400

    deploy attempt 1

--- src/Pages/MachineIntelligence.js
+++ src/Pages/MachineIntelligence.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Spline from '@splinetool/react-spline';
 
 const MachineIntelligence = () => {
-
-
     return (
         <Spline scene="https://prod.spline.design/jpi11Nh8wnGc9BM2/scene.splinecode" />
     );
