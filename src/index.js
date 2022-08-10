351d3c15f905eaee0fafd54b57beccb194330627
Sarah <lemiex@outlook.com>
2022-08-10 12:12:51 -0400

    Back to App.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './styles.css';
-import App from './App1';
+import App from './App';
 import MachineIntelligence from './Pages/MachineIntelligence';
 import Experiences from './Pages/Experiences';
 import Sustainability from './Pages/Sustainability';
