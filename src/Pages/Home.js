2b8318601185e21f9efd28f4b6f8fd47cc3596f5
Sarah <lemiex@outlook.com>
2022-08-20 10:57:36 -0400

    added home page, separate from App.js

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+const Home = () => {
+    return (
+        <div className="homePage" style={{ height: '100vh' }}>
+            <Spline scene="https://prod.spline.design/CZ9FJkxZ6rqLQU2k/scene.splinecode" />
+        </div>
+    )
+}
+
+export default Home
